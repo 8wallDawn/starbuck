313cf7f97a6993f2b8cc47a4d12cedba7c4d4358
8wallDawn <dankthedust@gmail.com>
2021-09-18 17:48:57 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-//HEADER SEARCH BAR
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 //BADGES DISPLAY ON OFF
 const badgesEl = document.querySelector('header .badges');
 //BADGES DISPLAY ON OFF
@@ -171,4 +153,3 @@ new Swiper('.awards .swiper', {
         nextEl: '.awards .swiper-next'
     }
 });
-
